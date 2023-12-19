40c5eb5b2deb665d3d754a619a6cbf32c148d2f5
andrestrw <133chicagobulls@gmail.com>
2023-12-19 13:38:59 +0100

    Connections terminated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const socket = io("/");
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+
+    setMessages([...messages, newMessage]);
+    socket.emit("message", message);
+  };
+
+  useEffect(() => {
+    socket.on("message", receiveMessage);
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages((state) => [...state, message]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Write wour message..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
+      <ul>
+        {messages.map((message, i) => (
+          <li key={i}>
+            {message.from}:{message.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -6,5 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("message", (body) => {
+    console.log(body);
+    socket.broadcast.emit("message", { body, from: socket.id.slice(6) });
+  });
+});
+
 server.listen(4000);
 console.log("Server on port", 4000);
